8a4abb598ea68277fb93d11bcb82e3b2782800d3
Segundo Ferreira <segundo.ferreira28@gmail.com>
2025-04-25 18:56:53 +0000

    proyecto terminado

--- src/js/Semaforo.jsx
+++ src/js/Semaforo.jsx
@@ -5,35 +5,38 @@ import React, { useState, useEffect } from "react";
 
 export const Traffic = () => {
 
-    const colors = ["yellow", "green", "red"];
+    
 
-    const [color, setColor] = useState(colors);
+    const [color1, setColor] = useState("red");
+    const [color2, setColor2] = useState("yellow");
+    const [color3, setColor3] = useState("green");
+    const [button, setButton] = useState("red");
 
     return (
         <>
 
             <div className="Semaforo">
-                <button className="rounded-circle"
+            <button className={`rounded-circle ${button === 'red' ? 'active' : 'none'}`}
                     onClick={() => {
-                        const index = Math.floor(Math.random() * colors.length)
-                        setColor(colors[index])
+                        setColor("red")
+                        setButton("red")
                     }
                     }
-                    style={{ backgroundColor: color, width: '130px', height: '130px' }}></button>
-                    <button className="rounded-circle"
+                    style={{ backgroundColor: color1, width: '130px', height: '130px', boxShadow: button === 'red' ? '0 0 40px blue' : 'none'}}></button>
+                    <button className={`rounded-circle ${button === 'yellow' ? 'active' : 'none'}`}
                     onClick={() => {
-                        const index = Math.floor(Math.random() * colors.length)
-                        setColor(colors[index])
+                        setButton("yellow")
+                        setColor2("yellow")
                     }
                     }
-                    style={{ backgroundColor: color, width: '130px', height: '130px' }}></button>
-                    <button className="rounded-circle"
+                    style={{ backgroundColor: color2, width: '130px', height: '130px', boxShadow: button === 'yellow' ? '0 0 40px violet' : 'none' }}></button>
+                    <button className={`rounded-circle ${button === 'green' ? 'active' : 'none'}`}
                     onClick={() => {
-                        const index = Math.floor(Math.random() * colors.length)
-                        setColor(colors[index])
+                        setButton("green")
+                        setColor3("green")
                     }
                     }
-                    style={{ backgroundColor: color, width: '130px', height: '130px' }}></button>
+                    style={{ backgroundColor: color3, width: '130px', height: '130px', boxShadow: button === 'green' ? '0 0 40px brown' : 'none' }}></button>
                 
 
             </div>
